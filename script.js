54f50cab58036bd2786a573056df235d11f8d561
afrabouzehar <bouzehar.afra@gmail.com>
2026-03-24 21:58:36 +0100

    deleting else

--- script.js
+++ script.js
@@ -5,19 +5,20 @@ function checking() {
     let UtilisateurGuess = document.getElementById('deviner').value;
     let message = document.getElementById('message');
     tentatives++;
-    
+
     if (UtilisateurGuess == randomNumber) {
-        message.innerHTML = `Félicitations! Vous avez trouvé le nombre ${randomNumber} en ${tentatives} .`;
+        message.innerHTML = `Félicitations! Vous avez trouvé le nombre ${randomNumber} en ${tentatives} tentatives .`;
         message.style.color = 'green';
-    }
-    else if (UtilisateurGuess > randomNumber) {
-        message.innerHTML = `Le nombre est plus grand`;
-        message.style.color = `red`;
     }
     else if (UtilisateurGuess < randomNumber) {
         message.innerHTML = `Le nombre est plus petit`;
         message.style.color = `red`;
+    } else if (UtilisateurGuess > randomNumber) {
+        message.innerHTML = `Le nombre est plus grand`;
+        message.style.color = `red`;
     }
+
 }
+
 
 
